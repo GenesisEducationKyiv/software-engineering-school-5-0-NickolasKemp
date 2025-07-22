faaa0d1e4d0fbc963c562a2b69ea08261f17c4d5
@mykola.tubolev <mykola.tubolev@techmagic.co>
2025-07-22 16:51:29 +0300

    refactor: [hw9 & hw10] remove unused

--- apps/notification-sender/test/app.e2e-spec.ts
+++ apps/notification-sender/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { NotificationSenderModule } from '../src/infrastructure/notification-sender.module';
+import { Http2Server } from 'http2';
 
 describe('NotificationSenderController (e2e)', () => {
   let app: INestApplication;
@@ -16,6 +17,9 @@ describe('NotificationSenderController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    return request(app.getHttpServer() as Http2Server)
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
   });
 });
--- apps/weather-api/test/mocks/logger.mock.ts
+++
@@ -1,7 +0,0 @@
-export const mockLogger = {
-  log: jest.fn(),
-  error: jest.fn(),
-  warn: jest.fn(),
-  debug: jest.fn(),
-  verbose: jest.fn(),
-};
