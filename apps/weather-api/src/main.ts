e9788a8846273dd7e387640390ea9ae1d2f6c9a0
@mykola.tubolev <mykola.tubolev@techmagic.co>
2025-07-27 18:02:22 +0300

    feat: [hw11] add observability services

--- apps/weather-api/src/main.ts
+++ apps/weather-api/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@shared/infrastructure/logger';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new Logger('Weather API'),
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
